40fd53ef5ac09e690f83d9b5d7846a0d7547bc39
hyunsu00 <hyun955807@naver.com>
2023-06-15 01:24:59 +0900

    코드 수정

--- commonSample/main.cpp
+++ commonSample/main.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
-// #include "HncZLib.h"
+#include "HncZLib.h"
 
 int main() {
   printf("calling HncGZOpenRead(nullptr, nullptr)\n");
 
-  // HncGZOpenRead(nullptr, nullptr);
+  HncGZOpenRead(nullptr, nullptr);
 
   printf("called HncGZOpenRead(nullptr, nullptr)\n");
   return 0;
