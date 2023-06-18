77a5ee75e3cae1395205c5517fb07721cc87ac3e
hyunsu00 <hyun955807@naver.com>
2023-06-19 00:36:59 +0900

    코드추가

--- caesar/src/caesar.cpp
+++ caesar/src/caesar.cpp
@@ -1,22 +1,28 @@
-typedef long int i32;
-#include <stdint.h>
-#include <stdio.h>
+// caesar.cpp
+#include <stdint.h> // int32_t
+#include <stdio.h> // printf
+#include <string> // std::string
 
-// int main()
-// {
-//   printf("caesar test\n");
-
-//   return 0;
-// }
+int main()
+{
+  std::string str1 = "1234";
+	str1 += "5678";
+  printf("%s\n", str1.c_str());
+  printf("caesar test\n");
+  return 0;
+}
 
 extern "C" {
-  void caesarEncrypt(i32 *plaintext, i32 plaintextLength, i32 key) {
+  void caesarEncrypt(int32_t* plaintext, int32_t plaintextLength, int32_t key) {
+    std::string str1 = "1234";
+	  str1 += "5678";
+    printf("%s\n", str1.c_str());
     for (int i = 0; i < plaintextLength; i++) {
       plaintext[i] = (plaintext[i] + key) % 26;
     }
   }
 
-  void caesarDecrypt(i32 *ciphertext, i32 ciphertextLength, i32 key) {
+  void caesarDecrypt(int32_t* ciphertext, int32_t ciphertextLength, int32_t key) {
     for (int i = 0; i < ciphertextLength; i++) {
       ciphertext[i] = (ciphertext[i] - key) % 26;
     }
